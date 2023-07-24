0f1fd627e2a1ad4997a941dd7e79d992541e7f5b
ftoutounchi <f.tutunchi@gmail.com>
2023-07-24 21:53:03 +0200

    modify code

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -1,12 +1,12 @@
 public class AnagramChecker {
 
-    // Helper method to remove non-letter characters and convert to lowercase
-    private static String cleanText(String text) {
+    // method to remove non-letter characters and convert to lowercase
+    private String cleanText(String text) {
         return text.replaceAll("[^a-zA-Z]", "").toLowerCase();
     }
 
-    // Method to check if two texts are anagrams
-    public static boolean areAnagrams(String text1, String text2) {
+    // to check if two texts are anagrams
+    public boolean areAnagrams(String text1, String text2) {
         // Clean the input texts by removing non-letter characters and converting to
         // lowercase
         String cleanText1 = cleanText(text1);
@@ -37,16 +37,5 @@ public class AnagramChecker {
         }
 
         return true; // If all characters in text2 are accounted for, they are anagrams.
-    }
-
-    public static void main(String[] args) {
-        String text1 = "listen";
-        String text2 = "silent";
-
-        if (areAnagrams(text1, text2)) {
-            System.out.println("The texts are anagrams.");
-        } else {
-            System.out.println("The texts are not anagrams.");
-        }
     }
 }
